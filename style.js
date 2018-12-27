8406e3b3fd59e0dfca1fcefa85fede7e43238ae2
Elena Vieru <33833374+elenavieru@users.noreply.github.com>
2018-12-27 16:11:53 +0100

    Update style.js

--- style.js
+++ style.js
@@ -118,9 +118,9 @@
 				{ name: 'Desplegable', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'santillana_ingles-dropdown' } },
 				{ name: 'Desplegable 2', type: 'widget', widget: 'blink_dropdown', attributes: { 'class': 'santillana_ingles-dropdown-2' } },
 				
-				{ name: 'Icono Step 1', element: 'span', attributes: { 'class': 'icon icon-antes' } },
-				{ name: 'Icono Step 2', element: 'span', attributes: { 'class': 'icon icon-durante' } },
-				{ name: 'Icono Step 3', element: 'span', attributes: { 'class': 'icon icon-despues' } }
+				{ name: 'Icono Step 1', element: 'span', attributes: { 'class': 'icon icon-1' } },
+				{ name: 'Icono Step 2', element: 'span', attributes: { 'class': 'icon icon-2' } },
+				{ name: 'Icono Step 3', element: 'span', attributes: { 'class': 'icon icon-3' } }
 
 
 			]
