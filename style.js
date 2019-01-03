5eb5a47afe2d6504cac21f423b65ae5d34809ccb
Elena Vieru <33833374+elenavieru@users.noreply.github.com>
2019-01-03 14:55:24 +0100

    Update style.js

--- style.js
+++ style.js
@@ -100,6 +100,7 @@
 				{ name: 'Social responsibility', element: 'span', attributes: { 'class': 'icon icon-11'} },
 				{ name: 'Connect', element: 'span', attributes: { 'class': 'icon icon-12'} },
 				{ name: 'Writing Skills', element: 'span', attributes: { 'class': 'icon icon-13'} },
+				{ name: 'Cultural', element: 'span', attributes: { 'class': 'icon icon-14'} },
 				{ name: 'Icono Step 1', element: 'span', attributes: { 'class': 'icon icon-1' } },
 				{ name: 'Icono Step 2', element: 'span', attributes: { 'class': 'icon icon-2' } },
 				{ name: 'Icono Step 3', element: 'span', attributes: { 'class': 'icon icon-3' } },
